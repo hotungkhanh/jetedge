0e9570b16d929998b6f27f3593f6a804a938fc3c
Giang Vu <dhgiang.vu@gmail.com>
2024-08-22 15:59:07 +1000

    feat: implement spreadsheet input pages with JSpreadsheet CE
    implement spreadsheet's persistence via sessionStorage

--- frontend/src/pages/spreadsheets/Room.tsx
+++ frontend/src/pages/spreadsheets/Room.tsx
@@ -1,3 +1,14 @@
+import Spreadsheet from '../../components/Spreadsheet.tsx'
+
 export default function Room() {
-  return <h3>Room</h3>;
-}
+
+  return (
+    <>
+      <h3>Room</h3>
+      <Spreadsheet 
+        headers={["Campus Name", "Building Name", "Room Code", "Available Time (start)", "Available Time (end)"]}
+        storageKey='room'
+      />
+    </>
+  );
+};
