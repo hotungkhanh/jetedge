3c099f2bac49c677c930fe51bc1f090bd3ba6d74
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-08-22 15:09:58 +1000

    feat: add sidebar component

--- frontend/src/pages/SemesterInfo.tsx
+++ frontend/src/pages/SemesterInfo.tsx
@@ -1,19 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
-
+import Sidebar from "../components/Sidebar.tsx";
 export default function SemesterInfo() {
   return (
     <>
-      {/* Replace this with Sidebar component */}
-      <div style={{ backgroundColor: "aqua", position: "absolute", width: 240 + "px" }}>
-        <h2>Scuffed Sidebar :D <br /> (replace pls)</h2>
-        <ul>
-          <li><Link to="campus">Campus</Link></li>
-          <li><Link to="building">Building</Link></li>
-          <li><Link to="room">Room</Link></li>
-          <li><Link to="unit">Unit</Link></li>
-        </ul>
-      </div>
-
+      <Sidebar></Sidebar>
       <div style={{ backgroundColor: "bisque", marginLeft: 240 + "px", paddingBottom: 50+"px" }}>
         <h1>Spreadsheet Container</h1>
         <Outlet />
