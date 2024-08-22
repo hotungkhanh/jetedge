c303fea57905b3dd2493bfbb430ec621c7e8714b
Giang Vu <dhgiang.vu@gmail.com>
2024-08-22 15:59:07 +1000

    feat: implement spreadsheet input pages with JSpreadsheet CE
    implement spreadsheet's persistence via sessionStorage

--- frontend/src/pages/spreadsheets/Campus.tsx
+++ frontend/src/pages/spreadsheets/Campus.tsx
@@ -1,3 +1,11 @@
+import Spreadsheet from '../../components/Spreadsheet.tsx'
+
 export default function Campus() {
-  return <h3>Campus</h3>;
-}
+
+  return (
+    <>
+      <h3>Campus</h3>
+      <Spreadsheet headers={["Campus Name"]} storageKey='campus' />
+    </>
+  );
+};
