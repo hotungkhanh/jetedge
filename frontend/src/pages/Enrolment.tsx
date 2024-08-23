e1aa2bc1584d76945c7789160e12897931caa7e9
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-08-23 18:13:28 +1000

    feat: Add Header and Footer
    header and footer are now available for use in Header.tsx and Footer.tsx

--- frontend/src/pages/Enrolment.tsx
+++ frontend/src/pages/Enrolment.tsx
@@ -1,15 +1,17 @@
 import { Box } from "@mui/material";
 import "../styles/enrolment.css";
 import UploadPopUp from "../components/UploadPopUp.tsx";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 export default function StarterPage() {
   return (
     <Box className="app-container">
-      <Box className="header"></Box>
+      <Header/>
       <Box className="content">
         <UploadPopUp></UploadPopUp>
       </Box>
-      <Box className="footer"></Box>
+      <Footer/>
     </Box>
   );
 }
