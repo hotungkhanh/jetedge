52528fb8baec9ce60f4007c72468b5d954e9cf33
Giang Vu <dhgiang.vu@gmail.com>
2024-08-23 20:27:58 +1000

    feat: add button components for navigating between pages

--- frontend/src/pages/SemesterInfo.tsx
+++ frontend/src/pages/SemesterInfo.tsx
@@ -2,19 +2,26 @@ import { Link, Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar.tsx";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
+import '../styles/seminfo.css';
+import NextButton from "../components/NextButton.tsx";
+import BackButton from "../components/BackButton.tsx";
 
 export default function SemesterInfo() {
   return (
     <div>
       <Header />      
       <Sidebar width={240} marginTop={12.5}/>
+
+      {/* Spreadsheet */}
       <div style={{ marginLeft: 300 + "px", overflow: "scroll", height: "550px" }}>
         <Outlet />
       </div>
 
       <Footer>
-        <Link to="../" style={{ marginLeft: 240+"px" }}>Go Back</Link>
-        <Link to="../timetablemod" style={{ marginLeft: 240+"px" }}>Go to Next</Link>
+        <div className="links-container">
+          <Link to="../"><BackButton/></Link>
+          <Link to="../timetablemod"><NextButton/></Link>
+        </div>
       </Footer>
 
     </div>
