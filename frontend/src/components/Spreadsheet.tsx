362f769f2ab9dd8efa61362b7dae967393653e2c
Giang Vu <dhgiang.vu@gmail.com>
2024-08-24 15:24:02 +1000

    feat: add Jspreadsheet contextMenu + table overflow

--- frontend/src/components/Spreadsheet.tsx
+++ frontend/src/components/Spreadsheet.tsx
@@ -1,7 +1,8 @@
 import { useRef, useEffect } from "react";
-import jspreadsheet, { CellValue, JspreadsheetInstanceElement } from "jspreadsheet-ce";
+import jspreadsheet, { CellValue, JspreadsheetInstance, JspreadsheetInstanceElement, JSpreadsheetOptions } from "jspreadsheet-ce";
 import "../../node_modules/jspreadsheet-ce/dist/jspreadsheet.css";
 import "../styles/spreadsheet.css"
+import Button from "@mui/material/Button";
 
 interface SpreadsheetProps {
   headers: string[];
@@ -19,7 +20,60 @@ export default function Spreadsheet({ headers, storageKey }: SpreadsheetProps) {
   const columns = headers.map((headerName) => {
     return { title: headerName, width: 200 };
   });
-  
+
+  // spreadsheet init options: contextMenu property
+  const contextMenu = (instance: JspreadsheetInstance, _col: string | null, row: string | null, _evn: PointerEvent) => {
+
+    var items: object[] = [];
+    if (row === null) {
+      return items;
+    }
+
+    if (!instance.options.text) {
+      return items;
+    }
+
+    // insert row before
+    items.push({
+      title: instance.options.text.insertANewRowBefore,
+      onclick: function () {
+        instance.insertRow(1, parseInt(row), 1);
+      }
+    });
+
+    // insert row after
+    items.push({
+      title: instance.options.text.insertANewRowAfter,
+      onclick: function () {
+        instance.insertRow(1, parseInt(row));
+      }
+    });
+
+    // delete selected rows
+    items.push({
+      title: instance.options.text.deleteSelectedRows,
+      onclick: function () {
+        instance.deleteRow(instance.getSelectedRows().length ? undefined : parseInt(row));
+      }
+    });
+
+    // clear value of selected cells
+    items.push({
+      title: "Clear selected cells",
+      onclick: function () {
+        const selectedCols = instance.getSelectedColumns();
+        const selectedRows = instance.getSelectedRows(true) as number[];
+        for (let i of selectedCols) {
+          for (let j of selectedRows) {
+            instance.setValueFromCoords(i, j, "", false);
+          }
+        }
+      }
+    });
+    
+    return items;
+  };
+
   // spreadsheet init: options
   const options: jspreadsheet.JSpreadsheetOptions = {
     columns: columns,
@@ -27,7 +81,12 @@ export default function Spreadsheet({ headers, storageKey }: SpreadsheetProps) {
     minDimensions: [headers.length, 10],
     minSpareRows: 1,
     allowManualInsertColumn: false,
+    allowInsertColumn: false,
     includeHeadersOnDownload: true,
+    tableOverflow: true,
+    tableWidth: '1150px',
+    tableHeight: '400px',
+    contextMenu: contextMenu,
     // toolbar:[
     //   {
     //       type: 'i',
@@ -36,7 +95,16 @@ export default function Spreadsheet({ headers, storageKey }: SpreadsheetProps) {
     //       onclick: function() {
     //           undo();
     //       }
-    //   }],
+    //   },
+    //   {
+    //     type: 'i',
+    //     id: 'redo',
+    //     content: 'redo',
+    //     onclick: function() {
+    //         redo();
+    //     }
+    //   },
+    // ],
   };
 
   // mount: create spreadsheet using data from sessionStorage (if exist),
@@ -45,7 +113,7 @@ export default function Spreadsheet({ headers, storageKey }: SpreadsheetProps) {
     // console.log(`Mount ${storageKey}`);
     if (jRef.current && !jRef.current.jspreadsheet) {
       const savedSpreadsheetData = sessionStorage.getItem(storageKey);
-      
+
       if (savedSpreadsheetData) {
         const ssd: SavedSpreadsheetOpt = JSON.parse(savedSpreadsheetData);
         options.data = ssd.cellvalues;
@@ -77,21 +145,45 @@ export default function Spreadsheet({ headers, storageKey }: SpreadsheetProps) {
     return cleanup;
   })
 
-  // const addRow = () => {
-  //   if (jRef.current && jRef.current.jexcel) {
-  //     jRef.current.jexcel.insertRow();
-  //   }
-  // };
+  const addRow = () => {
+    if (jRef.current && jRef.current.jexcel) {
+      jRef.current.jexcel.insertRow();
+    }
+  };
 
   // const undo = () => {
   //   if (jRef.current && jRef.current.jexcel) {
   //     jRef.current.jexcel.undo();
   //   }
   // }
-  
+
+  // const redo = () => {
+  //   if (jRef.current && jRef.current.jexcel) {
+  //     jRef.current.jexcel.redo();
+  //   }
+  // }
+
   return (
     <div>
       <div ref={jRef} />
-    </div>
+      <br />
+      <Button
+        variant="outlined"
+        size="small"
+        onClick={addRow}
+        sx={{
+          color: "black",
+          borderColor: "black",
+          '&:hover': {
+            color: "#f05a22",
+            borderColor: "#f05a22",
+            backgroundColor: 'rgba(255, 255, 255, 0.1)',
+          },
+          borderRadius: "3px",
+          marginTop: "5px",
+        }}>
+        Add new row
+      </Button>
+    </div >
   );
 }
