97b7713241edfc4a398620066bc2cec45336ce9c
Eden Xu <eden.xu@uptickhq.com>
2024-08-29 14:53:33 +1000

    build: remove legacy Django backend and docker compose file

--- legacy-backend/campuses/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from ..models import Campus
-from .serializers import CampusSerializer
-
-class CampusViewSet(ModelViewSet):
-    queryset = Campus.objects.all()
-    serializer_class = CampusSerializer
