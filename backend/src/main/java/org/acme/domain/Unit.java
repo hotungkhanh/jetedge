fe949ae433cb07256875cb0a5368cda00313a9ff
tungkhanhh <tungkhanh.ho@student.unimelb.edu.au>
2024-09-13 22:24:25 +1000

    refactor: remove redundant methods from Unit class

--- backend/src/main/java/org/acme/domain/Unit.java
+++ backend/src/main/java/org/acme/domain/Unit.java
@@ -112,39 +112,6 @@ public class Unit {
     }
 
     /**
-     * Check if the unit has a common student with the other unit.
-     *
-     * @param otherUnit The other unit.
-     * @return A boolean representing whether there is a common student.
-     */
-    public boolean hasSameStudent(Unit otherUnit) {
-        for (Student student : students) {
-            if (otherUnit.getStudents().contains(student)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Get the number of common students between 2 units.
-     *
-     * @param otherUnit The other unit.
-     * @return An int representing the number of common students.
-     */
-    public int numSameStudent(Unit otherUnit) {
-        int num = 0;
-
-        for (Student student : students) {
-            if (otherUnit.getStudents().contains(student)) {
-                num++;
-            }
-        }
-        return num;
-    }
-
-    /**
      * Get the number of students enrolled in the unit.
      *
      * @return An int representing the number of students.
