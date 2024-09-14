bb2686f8ed28fac4300128c1d14ebe7939596cb3
hotungkhanh <hotungkhanh2003@gmail.com>
2024-09-14 20:16:43 +1000

    feat: add the day-of-week to Timetable

--- backend/src/main/java/org/acme/domain/Timetable.java
+++ backend/src/main/java/org/acme/domain/Timetable.java
@@ -7,6 +7,7 @@ import ai.timefold.solver.core.api.domain.solution.ProblemFactCollectionProperty
 import ai.timefold.solver.core.api.domain.valuerange.ValueRangeProvider;
 import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +21,14 @@ import java.util.List;
 public class Timetable {
 
     @PlanningEntityCollectionProperty
-    List<Unit> units;
+    private List<Unit> units;
 
     @ValueRangeProvider
-    List<LocalTime> startTimes;
+    private List<DayOfWeek> daysOfWeek;
+    @ValueRangeProvider
+    private List<LocalTime> startTimes;
     @PlanningScore
-    HardSoftScore score;
+    private HardSoftScore score;
     @ProblemFactCollectionProperty
     @ValueRangeProvider
     private List<Room> rooms;
@@ -56,6 +59,29 @@ public class Timetable {
         this.units = units;
         this.startTimes = startTimes;
         this.rooms = rooms;
+    }
+
+    /**
+     * Creates a timetable.
+     *
+     * @param units      The list of units to be allocated.
+     * @param daysOfWeek The list of available days of the week.
+     * @param startTimes The list of available starting times.
+     * @param rooms      The list of available rooms.
+     */
+    public Timetable(List<Unit> units, List<DayOfWeek> daysOfWeek, List<LocalTime> startTimes, List<Room> rooms) {
+        this.units = units;
+        this.daysOfWeek = daysOfWeek;
+        this.startTimes = startTimes;
+        this.rooms = rooms;
+    }
+
+    public List<DayOfWeek> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public void setDaysOfWeek(List<DayOfWeek> daysOfWeek) {
+        this.daysOfWeek = daysOfWeek;
     }
 
     public List<LocalTime> getStartTimes() {
