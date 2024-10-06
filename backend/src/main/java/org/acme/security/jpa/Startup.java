908cf9b520ff7cba7e5b762e691dc2151384bbbf
tungkhanhh <tungkhanh.ho@student.unimelb.edu.au>
2024-10-07 00:05:46 +1100

    feat: hide frontend username and password in .env

--- backend/src/main/java/org/acme/security/jpa/Startup.java
+++ backend/src/main/java/org/acme/security/jpa/Startup.java
@@ -1,18 +1,25 @@
 package org.acme.security.jpa;
 
+import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.transaction.Transactional;
 
-import io.quarkus.runtime.StartupEvent;
-
+import org.eclipse.microprofile.config.Config;
 
 @Singleton
 public class Startup {
+    @Inject
+    Config config;
+
     @Transactional
     public void loadUsers(@Observes StartupEvent evt) {
-        // reset and load all test users
+        String username = config.getValue("frontend.username", String.class);
+        String password = config.getValue("frontend.password", String.class);
+
+        // reset and load user
         User.deleteAll();
-        User.add("username", "pw", "user");
+        User.add(username, password, "user");
     }
 }
