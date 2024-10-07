83834850d0c6735d9076542dd0bd1469a9020ff8
hotungkhanh <hotungkhanh2003@gmail.com>
2024-10-08 00:27:05 +1100

    refactor: :recycle: refactor files, bring to "rest" package, reformat code

--- backend/src/main/java/org/acme/rest/StudentResource.java
+++ backend/src/main/java/org/acme/rest/StudentResource.java
@@ -1,15 +1,12 @@
-package org.acme.domain;
-
-import java.util.List;
+package org.acme.rest;
 
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.acme.domain.Student;
+
+import java.util.List;
 
 @Path("/students")
 public class StudentResource {
